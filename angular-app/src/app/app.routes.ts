68f52bd74a12c91c98de8a0da8fb077ba4d810ef
Markus Lima <markusmak@email.com>
2025-06-13 16:13:10 -0300

    definido rotas do front

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { IndexUser } from './modules/user/index/index';
 import { IndexFollowing } from './modules/following/index/index';
 import { IndexLog } from './modules/log/index/index';
+import { ShowLog } from './modules/log/show';
 
 export const routes: Routes = [
     { path: "user", component: IndexUser },
-    { path: "following", component: IndexFollowing },
+    { path: "user/:userName", component: IndexUser }, 
+    { path: "following/:userName", component: IndexFollowing },
     { path: "log", component: IndexLog },
+    { path: "log/:id", component: ShowLog },
     { path: '', redirectTo: 'user', pathMatch: 'full'},
     { path: '**', redirectTo: 'user'}
 ];
