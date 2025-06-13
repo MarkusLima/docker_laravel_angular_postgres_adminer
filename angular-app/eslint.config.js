6b670f9a0c42944de8948c48334a895526de2d67
Markus Lima <markusmak@email.com>
2025-06-13 16:53:57 -0300

    instalação do eslint

---
+++ angular-app/eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettier = require('eslint-config-prettier');
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      prettier: require('eslint-plugin-prettier'),
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettier, // Desabilita regras conflitantes com Prettier
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      'prettier/prettier': 'error', // Aplica Prettier como regra de ESLint
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+);
